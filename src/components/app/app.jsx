fea4d4a2d1d68e590167d2b768ca15e8e8e69ff7
Евгений Потапов <pea@uiex.com>
2020-10-06 10:07:18 +0300

    Выполнено задание Личный проект: маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
-import MainPage from "../main/main";
+import {Switch, Route, BrowserRouter, Redirect} from "react-router-dom";
+import Favorites from "../pages/favorites-page/favorites-page";
+import PageMain from "../pages/main-page/main-page";
+import Room from "../pages/room-page/room-page";
+import SignIn from "../pages/sign-in-page/sign-in-page";
 
 const App = (props) => {
   const {offersCount} = props;
 
   return (
-    <MainPage offersCount = {offersCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={() => <PageMain offersCount = {offersCount}/>}/>
+        <Route exact path="/login" component={SignIn} />
+        <Route exact path="/favorites" component={Favorites} />
+        <Route exact path="/offer/:id" component={Room}/>
+        <Redirect to="/"/>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
