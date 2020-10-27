ad064ac17d9ae354626ee068f83ef0cb7fa50de4
Евгений <foxspb2012@gmail.com>
2020-10-27 08:41:38 +0300

    Выполнено задание Личный проект: больше подробностей(часть 1)

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -3,6 +3,7 @@ import {OfferType} from '../const';
 export const offers = [
   {
     id: 1,
+    city: `Amsterdam`,
     title: `Canal View Prinsengracht`,
     description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
     type: OfferType.APARTMENT,
@@ -18,10 +19,12 @@ export const offers = [
       name: `Steven`,
       avatar: `https://www.stevensegallery.com/640/360`,
       isSuper: true
-    }
+    },
+    coords: [52.3909553943508, 4.85309666406198]
   },
   {
     id: 2,
+    city: `Amsterdam`,
     title: `Nice, cozy, warm big bed apartment`,
     description: `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`,
     type: OfferType.HOUSE,
@@ -37,10 +40,12 @@ export const offers = [
       name: `Fill`,
       avatar: `https://www.fillmurray.com/640/360`,
       isSuper: true
-    }
+    },
+    coords: [52.369553943508, 4.85309666406198]
   },
   {
     id: 3,
+    city: `Amsterdam`,
     title: `Beautiful & luxurious apartment at great location`,
     description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
     type: OfferType.HOTEL,
@@ -56,10 +61,12 @@ export const offers = [
       name: `Cage`,
       avatar: `https://www.placecage.com/640/360`,
       isSuper: true
-    }
+    },
+    coords: [52.3909553943508, 4.929309666406198]
   },
   {
     id: 4,
+    city: `Amsterdam`,
     title: `Wood and stone place`,
     description: `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`,
     type: OfferType.PRIVATE_ROOM,
@@ -75,6 +82,7 @@ export const offers = [
       name: `Beard`,
       avatar: `https://placebeard.it/640x360`,
       isSuper: true
-    }
+    },
+    coords: [52.3809553943508, 4.939309666406198]
   }
 ];
